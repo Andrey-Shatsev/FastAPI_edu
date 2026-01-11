04261e275d54f51163e8e9c1d3cfe0f7858fca79
Andrey-Shatsev <a.g.shatsev@gmail.com>
2026-01-11 19:38:37 +0300

    feat(api) add file for testing post method and file for checking results

---
+++ src/api_test.py
@@ -0,0 +1,14 @@
+# для проверки API
+# обязательно передавать в json
+import requests
+import json
+
+body = {
+    "name": "Foo",
+    "description": "An optional description",
+    "price": 45.2,
+    "tax": 3.5
+}
+
+r = requests.post('http://127.0.0.1:8000/items/', data=json.dumps(body))
+print(str(r.status_code) + " " +  r.text)
