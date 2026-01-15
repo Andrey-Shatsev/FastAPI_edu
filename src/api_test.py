d1da1cc5d1b81843bf10cacf2721c4607be332d9
Andrey-Shatsev <a.g.shatsev@gmail.com>
2026-01-15 08:46:30 +0300

    feat(api): added file for request body example and test for it

--- src/api_test.py
+++ src/api_test.py
@@ -10,5 +10,5 @@ body = {
     "tax": 3.5
 }
 
-r = requests.post('http://127.0.0.1:8000/items/', data=json.dumps(body))
+r = requests.put('http://127.0.0.1:8000/items/1', data=json.dumps(body))
 print(str(r.status_code) + " " +  r.text)
